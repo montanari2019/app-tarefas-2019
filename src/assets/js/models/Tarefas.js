fdc269ee9acde0324bc383987e247aa92c707635
Ikaro Montanari <ikaro.montanari31@gmail.com>
2019-09-11 21:40:49 -0400

    Aula 02 cara eu to entendendo um pouco

---
+++ src/assets/js/models/Tarefas.js
@@ -0,0 +1,17 @@
+class Tarefas{
+  constructor(){
+    this._tarefas = []
+  }
+
+  adicionar(tarefa){
+    this._tarefas.push(tarefa)
+  }
+
+  listar(){
+    return this._tarefas
+  }
+
+  limpar(){
+    this,this._tarefas = []
+  }
+}
